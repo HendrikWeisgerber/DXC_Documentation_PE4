1fffa8e22c4b1837c31801644d97ecf69122b91c
HendrikWeisgerber <57496555+HendrikWeisgerber@users.noreply.github.com>
2021-10-12 11:31:55 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #echo "###########################################Installing Chrome"
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #echo "franka-DXC" | sudo -S apt install ./google-chrome-stable_current_amd64.deb -y
